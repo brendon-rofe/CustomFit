c654adea55ce2b79734d229114756aea28d6502d
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-05-05 11:49:38 +0200

    feat: Setup initial Gemini connection

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
+import google.generativeai as genai
 
 load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=gemini_api_key)
+model = genai.GenerativeModel("gemini-2.5-flash-preview-04-17")
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to CustomFit API!"}
+
+@app.get("/ask")
+def ask_ai(prompt: str):
+  response = model.generate_content(prompt)
+  return {"response": response.text}
