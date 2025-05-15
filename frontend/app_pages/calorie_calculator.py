1e6af306daaad612ebdb5097a882328b03b30375
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-05-15 12:12:45 +0200

    feat: Implement initial calorie and macro calculation on app

--- frontend/app_pages/calorie_calculator.py
+++ frontend/app_pages/calorie_calculator.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.header("Daily Calorie & Macro Calculator")
 
@@ -10,4 +11,11 @@ height = st.number_input("What is your height? (In meters)", value=None, placeho
 
 weight = st.number_input("How much do you weigh? (In kilograms)", value=None, placeholder=0)
 
-st.button("Submit")
+if st.button("Submit"):
+  with st.spinner("Calculating..."):
+    response = requests.post("http://localhost:8000/calorie-calculator", json={"gender": gender, "age": age, "height": height, "weight": weight})
+    st.success("Here are your total daily calories and macros:")
+    st.write("Total daily calories:", response.json()["calories"])
+    st.write("Total daily protein:", response.json()["protein"])
+    st.write("Total daily carbs:", response.json()["carbs"])
+    st.write("Total daily fat:", response.json()["fat"])
